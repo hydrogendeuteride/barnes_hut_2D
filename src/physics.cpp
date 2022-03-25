f21b658b58c9dd014525e898b4e45e7c5e2e0382
hydrogen_deuteride <dumpsterowner@gmail.com>
2022-03-25 01:55:37 +0000

    error correction in physics->func treetraversal

--- src/physics.cpp
+++ src/physics.cpp
@@ -1,16 +1,32 @@
 #include "physics.hpp"
+#include <tuple>
+
+
+template <typename ... Ts, std::size_t ... Is>
+std::tuple<Ts...> sumT (std::tuple<Ts...> const & t1,
+                        std::tuple<Ts...> const & t2,
+                        std::index_sequence<Is...> const &)
+ { return { (std::get<Is>(t1) + std::get<Is>(t2))... }; }
+
+template <typename ... Ts>
+std::tuple<Ts...> operator+ (std::tuple<Ts...> const & t1,
+                             std::tuple<Ts...> const & t2)
+ { return sumT(t1, t2, std::make_index_sequence<sizeof...(Ts)>{}); }
 
 template<typename TreeData>
-void treeTraversal(Node<TreeData> *leafNode, Node<TreeData> *rootNode, double timestep, 
+std::tuple<vec2, vec2> treeTraversal(Node<TreeData> *leafNode, Node<TreeData> *rootNode, double timestep, 
                     std::function<std::tuple<vec2, vec2>(vec2 &p, vec2 &q, double leafMass,
-                     double rootMass, vec2 &dist_v, double timestep)> Integrator)
+                     double rootMass, vec2 &dist_v, double timestep)> Integrator, 
+                     std::tuple<vec2, vec2> res)
 {
     if (rootNode->hasLeaf)
     {
+        std::tuple<vec2, vec2> ret = std::make_tuple(vec2(0.0, 0.0), vec2(0.0, 0.0));
+
         vec2 leafCOM  = CalcCOM(leafNode);
         vec2 rootCOM = CalcCOM(rootNode);
 
-        vec2 dist_v = rootCOM - leafCOM;
+        vec2 dist_v = leafCOM - rootCOM;
         double dist = dist_v.norm();
 
         if ((rootNode->width / dist <= THETA) || rootNode->hasLeaf == false)
@@ -21,20 +37,18 @@ void treeTraversal(Node<TreeData> *leafNode, Node<TreeData> *rootNode, double ti
             double leafmass = leafNode->bodies.at(0).mass;
             double rootmass = CalcTotalMass(rootNode);
 
-            std::make_tuple(vel, pos) = Integrator(vel, pos, leafmass, rootmass, dist_v, timestep); 
-            
-            leafNode->bodies.at(0).x += pos;
-            leafNode->bodies.at(0).v += vel;
+            ret = Integrator(vel, pos, leafmass, rootmass, dist_v, timestep);            
+            return ret;
         }
         else 
         {
-            treeTraversal(leafNode, rootNode -> leaf0);
-            treeTraversal(leafNode, rootNode -> leaf1);
-            treeTraversal(leafNode, rootNode -> leaf2);
-            treeTraversal(leafNode, rootNode -> leaf3);
+            return treeTraversal(leafNode, rootNode -> leaf0, timestep, Integrator, res + ret);
+            return treeTraversal(leafNode, rootNode -> leaf1, timestep, Integrator, res + ret);
+            return treeTraversal(leafNode, rootNode -> leaf2, timestep, Integrator, res + ret);
+            return treeTraversal(leafNode, rootNode -> leaf3, timestep, Integrator, res + ret);
         }
     }
-}
+} // O(log N), recursion optimized
 
 std::tuple<vec2, vec2> semi_implict_euler(const vec2 &v, const vec2 &x, 
                                             const double leafMass, const double rootMass,
--- src/physics.hpp
+++ src/physics.hpp
@@ -12,9 +12,10 @@ constexpr double G_CONST = 1.0;
 typedef Eigen::Vector2d vec2;
 
 template<typename TreeData>
-void treeTraversal(Node<TreeData> *leafNode, Node<TreeData> *rootNode, double timestep, 
+std::tuple<vec2, vec2> treeTraversal(Node<TreeData> *leafNode, Node<TreeData> *rootNode, double timestep, 
                     std::function<std::tuple<vec2, vec2>(vec2 &p, vec2 &q, 
-                    double leafMass, double rootMass, vec2 &dist_v, double timestep)> Integrator);
+                    double leafMass, double rootMass, vec2 &dist_v, double timestep)> Integrator,
+                    std::tuple<vec2, vec2> res = std::make_tuple(vec2(0.0, 0.0), vec2(0.0, 0.0)));
 
 std::tuple<vec2, vec2> semi_implict_euler(const vec2 &v, const vec2 &x, 
                                             const double leafMass, const double rootMass,
